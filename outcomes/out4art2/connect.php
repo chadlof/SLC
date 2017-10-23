6efd5b201727cb0e442e839b5785af5b8ef22e55
chadlof <chadlof@gmail.com>
2017-10-23 13:08:32 -0500

    1 and 1 connection update

--- outcomes/out4art2/connect.php
+++ outcomes/out4art2/connect.php
@@ -1,8 +1,8 @@
 <?php 
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_database = 'abcstone';
+$db_host = 'db702914424.db.1and1.com:3306';
+$db_user = 'dbo702914424';
+$db_pass = 'Password01';
+$db_database = 'db702914424';
 
 $db = new PDO('mysql:host='.$db_host.';
                 dbname=' .$db_database,
